9308f8538210946ee10e729a84a53f869b5f0367
Nirushana <kavinda.n.t@gmail.com>
2021-07-15 02:46:39 +0530

    fetch data

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { useSelector} from 'react-redux'
-import ProductComponent from './ProductComponent';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
+import axios from "axios";
 
 const ProductListing = () => {
-    const products = useSelector((state) => state);
-    console.log("Products: ", products);
-    return(
-        <div className="ui grid container" >
-            <ProductComponent />
-        </div>
-    )
-}
+  const products = useSelector((state) => state);
 
-export default ProductListing
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+      console.log("Response: ", response.data)
+  };
+
+  useEffect(() => {
+      fetchProducts();
+  }, [])
+  console.log("Products: ", products);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
+
+export default ProductListing;
