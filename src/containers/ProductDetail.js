44074154a6462eb61a15a8d2de6eae4fe74c2e70
Nirushana <kavinda.n.t@gmail.com>
2021-07-15 23:18:52 +0530

    productDetails Added

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectedProduct } from "../redux/actions/productActions";
 
 function ProductDetail() {
-    return (
-        <div>
-            <h1>ProductDetails</h1>
-        </div>
-    )
+  const product = useSelector((state) => state.product);
+  const { productId } = useParams();
+  console.log(product);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId && productId !== "") {
+      async function fetchProductDetail() {
+        const response = await axios
+          .get(`https://fakestoreapi.com/products/${productId}`)
+          .catch((err) => {
+            console.log("Err: ", err);
+          });
+        dispatch(selectedProduct(response.data));
+      }
+      fetchProductDetail();
+    }
+  }, [productId, dispatch]);
+  return (
+    <div>
+      <h1>ProductDetails</h1>
+    </div>
+  );
 }
 
-export default ProductDetail
+export default ProductDetail;
